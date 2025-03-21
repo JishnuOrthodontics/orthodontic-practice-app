67cdf270dce4e71a790102d340a7f01bf9743b28
Dr Jishnu S <jishnu262@gmail.com>
2025-03-21 12:28:32 +0530

    Setup environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCnFV-RGKJI6AXui_SsVynFfanRxOSdk9I",
-  authDomain: "ortho-website-project.firebaseapp.com",
-  projectId: "ortho-website-project",
-  storageBucket: "ortho-website-project.firebasestorage.app",
-  messagingSenderId: "920028436910",
-  appId: "1:920028436910:web:fac855372493a164cfd03f"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
